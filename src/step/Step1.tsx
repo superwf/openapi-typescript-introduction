de2c88f1600326716695c289651bcfe5a59b47e5
superwf <superwf@gmail.com>
2020-04-29 20:15:59 +0800

    build: init impress

---
+++ src/step/Step1.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export const Step1 = () => {
+  return <div className="step" data-x="-1000" data-y="0">step 1</div>
+}
---
+++ src/step/Step2.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export const Step2 = () => {
+  return <div className="step" data-x="0" data-y="0">step 2</div>
+}
