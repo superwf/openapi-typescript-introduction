67bb46347e60b6a6f9f89a6ce2fe1f24718237a4
wangfan15 <wangfan15@jd.com>
2020-09-01 16:19:41 +0800

    build

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@ ReactDOM.render(
 )
 
 if (process.env.NODE_ENV === 'production') {
-  document.body.style.backgroundImage = '/wangfan15/swagger-generator/public/asset/universe.jpg'
+  document.body.style.backgroundImage = 'url(/wangfan15/swagger-generator/public/asset/universe.jpg)'
 }
 
 // If you want your app to work offline and load faster, you can change
